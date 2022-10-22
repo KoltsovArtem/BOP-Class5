86448c607e80d79b826f013bc3f0a69575b142db
MaxGolubev19 <golubevmaksim2004@mail.ru>
2022-10-23 01:06:40 +0300

    Task 1

--- Class6/Task1/Task1.cs
+++ Class6/Task1/Task1.cs
@@ -1,19 +1,51 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace Task1
 {
-    class UnsupportedOperation : Exception
+    // Передано недостаточное число аргументов
+    class NotEnoughArguments : Exception
     {
-        UnsupportedOperation(char op) : base($"Operation '{op}' is unsupported")
-        {
-        }
+        public NotEnoughArguments(int available) : base($"More than {available} arguments required") { }
     }
 
+    // Некорректность схемы преобразования
+    class UnsupportedOperation : Exception
+    {
+        public UnsupportedOperation(char op) : base($"Operation '{op}' is unsupported") { }
+    }
+
+    // Отсутствие входного файла
+    class MissingInputFile : Exception
+    {
+        public MissingInputFile(string fileName) : base($"{fileName} not found") { }
+    }
+
+    // Разное количество строк во входных файлах
+    class DifferentNumberOfLines : Exception
+    {
+        public DifferentNumberOfLines(int available, string fileName) : base($"More than {available} lines required in {fileName}") { }
+    }
+
+    // Некорректный формат строк во входных файлах
+    class IncorrectFormatOfLines : Exception
+    {
+        public IncorrectFormatOfLines(string line, int numberLine) : base($"Incorrect format in line {numberLine}: ${line}") { }
+    }
+
+    /* Несоответствие количества чисел в соответствующей 
+     * строке второго файла выполняемому преобразованию */
     class NotEnoughNumbers : Exception
     {
-        public NotEnoughNumbers(int available, string line) : base($"More than {available} numbers required in ${line}")
-        {
-        }
+        public NotEnoughNumbers(int available, string line, int numberLine) : base($"More than {available} numbers required in line {numberLine}: ${line}") { }
+    }
+
+    // Невозможность создать выходной файл
+    class UnableCreateFile : Exception
+    {
+        public UnableCreateFile(string fileName) : base($"Unable to create file {fileName}") { }
     }
 
     public class Task1
@@ -21,55 +53,193 @@ namespace Task1
         private static readonly ILogger<Task1> Logger =
             LoggerFactory.Create(builder => { builder.AddSimpleConsole(); }).CreateLogger<Task1>();
 
+        //Выполнить операцию+
         internal static int ApplyOperation(char op, int arg1, int arg2)
         {
-            return TODO<int>();
+            var functions = new Dictionary<char, Func<int, int, int>>
+            {
+                {'*', (x, y) => x * y},
+                {'/', (x, y) => x / y},
+            };
+
+            try
+            {
+                return functions[op](arg1, arg2);
+            }
+
+            catch (KeyNotFoundException)
+            {
+                throw new UnsupportedOperation(op);
+            }
         }
 
+        //Преобразование схемы+
         private static Func<List<int>, int> ApplySchema(string schema)
         {
             var ops = schema.ToCharArray();
+
             return args =>
             {
-                var res = 0;
-                TODO();
+                int res = args[0];
+
+                for (var i = 0; i < schema.Length; i++)
+                {
+                    res = ApplyOperation(ops[i], res, args[i + 1]);
+                }
+
                 return res;
             };
         }
 
-        private static string FormatLhs(string schema, string[] numbers)
+        //Форматирование операций+
+        public static string FormatLhs(string schema, string[] numbers)
         {
-            return TODO<string>();
+            var result = new StringBuilder();
+            result.Append(numbers[0]);
+
+            for (var i = 0; i < schema.Length; i++)
+            {
+                result.Append(schema[i]);
+                result.Append(numbers[i + 1]);
+            }
+
+            return result.ToString();
         }
 
-        internal static string ProcessString(string schema, string input)
+        // Обработка строки
+        internal static string ProcessString(string schema, string input, int line = -1)
         {
             var transformation = ApplySchema(schema);
             var numbers = input.Split(",");
-            var result = transformation(numbers.Select(int.Parse).ToList());
-            return FormatLhs(schema, numbers) + $"={result}";
+            var result = "";
+
+            try
+            {
+                result = $"={transformation(numbers.Select(int.Parse).ToList())}";
+            }
+
+            catch (DivideByZeroException)
+            {
+                result = " *** DIVBYZERO";
+            }
+
+            catch(ArgumentOutOfRangeException)
+            {
+                throw new NotEnoughNumbers(numbers.Length, input, line);
+            }
+
+            catch (FormatException)
+            {
+                throw new IncorrectFormatOfLines(input, line);
+            }
+
+
+            return FormatLhs(schema, numbers) + $"{result}";
         }
 
-        internal static void ProcessFiles(string schemasFile, string dataFile, string outputFile)
+        // Чтение файла
+        public static string[] ReadFile(string fileName)
         {
-            TODO();
+            try
+            {
+                var data = File.ReadAllLines(fileName);
+                return data;
+            }
+
+            catch (FileNotFoundException)
+            {
+                throw new MissingInputFile(fileName);
+            }
+        }
+
+        // Запись в файл
+        public static void WriteFile(string fileName, List<string> data)
+        {
+            try
+            {
+                var file = new StreamWriter(fileName);
+
+                foreach (var line in data)
+                {
+                    file.WriteLine(line);
+                }
+
+                file.Close();
+            }
+
+            catch (DirectoryNotFoundException)
+            {
+                throw new UnableCreateFile(fileName);
+            }
+        }
+
+        public static void CheckLengths(string[] schema, string schemaFile, string[] data, string dataFile)
+        {
+            if (schema.Length < data.Length)
+            {
+                throw new DifferentNumberOfLines(schema.Length, schemaFile);
+            }
+
+            if (data.Length < schema.Length)
+            {
+                throw new DifferentNumberOfLines(data.Length, dataFile);
+            }
+        }
+
+        // Обработка файлов
+        internal static void ProcessFiles(string schemaFile, string dataFile, string outputFile)
+        {
+            var schema = ReadFile(schemaFile);
+            var data = ReadFile(dataFile);
+            var result = new List<string>();
+
+            CheckLengths(schema, schemaFile, data, dataFile); 
+
+            for (int i = 0; i < schema.Length; i++)
+            {
+                result.Add(ProcessString(schema[i], data[i], i));
+            }
+
+            WriteFile(outputFile, result);
+        }
+
+        // Обработка аргументов
+        public static (string, string, string) ParseArgs(string[] args)
+        {
+            try
+            {
+                var schemaFile = args[0];
+                var dataFile = args[1];
+                var outputFile = args[2];
+
+                return (schemaFile, dataFile, outputFile);
+            }
+
+            catch (IndexOutOfRangeException)
+            {
+                throw new NotEnoughArguments(args.Length);
+            }
         }
 
         public static void Main(string[] args)
         {
             Logger.LogInformation("program started");
-            TODO();
-            Logger.LogInformation("program completed");
-        }
 
-        private static void TODO()
-        {
-            throw new NotImplementedException();
-        }
+            try
+            {
+                var (schemaFile, dataFile, outputFile) = ParseArgs(args);
+                ProcessFiles(schemaFile, dataFile, outputFile);
+            }
 
-        private static T TODO<T>()
-        {
-            throw new NotImplementedException();
+            catch (Exception exception)
+            {
+                Logger.LogError(exception.Message);
+            }
+
+            finally
+            {
+                Logger.LogInformation("program completed");
+            }
         }
     }
 }
--- Class6/Task1/Task1Test.cs
+++ Class6/Task1/Task1Test.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System.IO.Enumeration;
 using static NUnit.Framework.Assert;
 using static Task1.Task1;
 
@@ -6,13 +7,69 @@ namespace Task1;
 
 public class Tests
 {
-    
+
     [Test]
     public void ApplyOperationTest()
     {
         That(ApplyOperation('*', 5, 2), Is.EqualTo(10));
         That(ApplyOperation('/', 10, 5), Is.EqualTo(2));
-        Catch(() => { ApplyOperation('/', 1, 0); });
-        Catch<UnsupportedOperation>(() => { ApplyOperation('/', 1, 0); });
+        Catch<DivideByZeroException>(() => { ApplyOperation('/', 1, 0); });
+    }
+
+    [Test]
+    public void FormatLhsTest()
+    {
+        That(FormatLhs("**/", new string[] { "1", "2", "3", "4" }), Is.EqualTo("1*2*3/4"));
+    }
+
+    [Test]
+    public void ProcessStringTest()
+    {
+        That(ProcessString("**", "1,2,3"), Is.EqualTo("1*2*3=6"));
+        That(ProcessString("**/", "1,2,3,4"), Is.EqualTo("1*2*3/4=1"));
+        Catch<NotEnoughNumbers>(() => { ProcessString("***", "12"); });
+    }
+
+    [Test]
+    public void ReadFileTest()
+    {
+        That(ReadFile("../../../schema_test.txt"), Is.EqualTo(new string[] { "123", "45", "6" }));
+        Catch<MissingInputFile>(() => { ReadFile("a.txt"); });
+    }
+
+    [Test]
+    public void WriteFileTest()
+    {
+        var fileName = "../../../schema_test.txt";
+        var data = new List<string> { "1,2,3", "4,5", "6" };
+
+        WriteFile(fileName, data);
+        That(ReadFile(fileName), Is.EqualTo(data));
+
+        Catch<UnableCreateFile>(() => { WriteFile("E/a.txt", new List<string> { "a" }); });
+    }
+
+    [Test]
+    public void ProcessFilesTest()
+    {
+        var schemaFile = "../../../data/schema.txt";
+        var dataFile1 = "../../../data/data.txt";
+        var dataFile2 = "../../../data/data2.txt";
+        var dataFile3 = "../../../data/data3.txt";
+        var dataFile4 = "../../../data/data4.txt";
+        var outputFile = "../../../data/output.txt";
+
+        ProcessFiles(schemaFile, dataFile1, outputFile);
+        That(ReadFile(outputFile), Is.EqualTo(new string[] { "2*4/3=2", "5*2*1*2=20", "1/0 *** DIVBYZERO", "5/5*2=2" }));
+        Catch<IncorrectFormatOfLines>(() => { ProcessFiles(schemaFile, dataFile2, outputFile); });
+        Catch<NotEnoughNumbers>(() => { ProcessFiles(schemaFile, dataFile3, outputFile); });
+        Catch<DifferentNumberOfLines>(() => { ProcessFiles(schemaFile, dataFile4, outputFile); });
+    }
+
+    [Test]
+    public void ParseArgsTest()
+    {
+        That(ParseArgs(new string[] { "1", "2", "3", "4" }), Is.EqualTo(("1", "2", "3")));
+        Catch<NotEnoughArguments>(() => { ParseArgs(new string[] { "a.txt" }); });
     }
 }
